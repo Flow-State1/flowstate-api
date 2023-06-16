31c56f3a8b8d8261adc08637b1eb921f0c649d89
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2023-06-16 14:08:54 +0200

    Added model on API and endpoint

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const mongoose = require("mongoose");
 const db =
   "mongodb+srv://dswfullstack:FullstackDSW2023@flowstate.1gxjqhm.mongodb.net/flowstate?retryWrites=true&w=majority";
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
 var cors = require("cors");
 const publish = require("./router/publisherRouter");
 const subscribe = require("./router/subscriberRouter");
+const Figures = require("./models/powerFigures")
 
 //Connecting to the db
 mongoose
@@ -23,3 +24,28 @@ mongoose
 app.use([cors()]);
 app.use("/publish", publish);
 app.use("/subscribe", subscribe);
+
+//Adding recorded unit to database
+app.post('/api/record', (req, res, next) => {
+  const figure = new Figures({
+    kWh: req.body.KWh,
+    date: req.body.date
+  });
+  figure.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+// app.listen(port, () => {
+//   console.log("Server is listening on port ${port}");
+// })
