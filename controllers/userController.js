290f224feaf6d1ce74c770acf2ad0cafb8829b3d
Pule-Prince <pulemabote01@gmail.com>
2023-06-30 02:37:59 +0200

    Added user model, issues cresting testing user API

---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+const User = require('./../models/user');
+
+//Functions that perform CRUD operations based on users
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined'
+  });
+};
+  
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined'
+  });
+};
+  
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined'
+  });
+};
+  
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined'
+  });
+};
+  
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined'
+  });
+};
+
+module.exports = {getAllUsers, getUser, createUser, updateUser, deleteUser}
---
+++ router/userRouter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const userRouter = express.Router();
+const userController = require("./../controllers/userController");
+const authController = require("./../controllers/authController");
+
+//Endpoint relevant for authentication
+userRouter.post('/signup', authController.signup);
+
+//Endpoint retrieves all users from the DB and creates a user
+userRouter
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+//Enpoint retrieves, updates, and deletes a specific user based on their id
+userRouter
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = userRouter;
