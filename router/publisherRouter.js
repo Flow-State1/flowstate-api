cbbcc595ed037d7a1b4ae5f79bd87c6336e0349c
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2023-09-17 14:38:18 +0200

    Adding env variables manually

--- router/publisherRouter.js
+++ router/publisherRouter.js
@@ -2,10 +2,19 @@ const router = require("express").Router();
 const publisherController = require("../controllers/publisherController");
 const publisher = require("../config/MQTTConnection");
 require("dotenv").config();
-const toggle_switch1 = process.env.SWITCH_PUBLISHERTOPIC1.toString();
-const switch_command1 = process.env.SWITCH_MESSAGE1.toString();
-const toggle_switch2 = process.env.SWITCH_PUBLISHERTOPIC2.toString();
-const switch_command2 = process.env.SWITCH_MESSAGE2.toString();
+
+// const toggle_switch1 = process.env.SWITCH_PUBLISHERTOPIC1.toString();
+// const switch_command1 = process.env.SWITCH_MESSAGE1.toString();
+// const toggle_switch2 = process.env.SWITCH_PUBLISHERTOPIC2.toString();
+// const switch_command2 = process.env.SWITCH_MESSAGE2.toString();
+
+//Manually inputting variables from env to avoid errors when hosting
+
+const toggle_switch1 = "shellyplus1pm-a8032ab11964/command/switch:0";
+const switch_command1 = "toggle";
+const toggle_switch2 = "shellyplus1pm-7c87ce719ccc/command/switch:0";
+const switch_command2 = "toggle";
+
 
 
 router.post("/", publisherController.publish_message_post);
